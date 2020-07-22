365318bf28d4a1ef8e51b49b5808b68cd6eb759a
Manny Lou <mavkio1@gmail.com>
2020-07-22 02:22:33 -0400

    edit answer button, created timer, endquiz, and highscores functions

--- Assets/script.js
+++ Assets/script.js
@@ -3,8 +3,11 @@
 var cardHeader = document.querySelector(".card-header");
 var cardBody = document.querySelector(".card-body");
 
-// Value of Current Questions.
-var currentQuestion = -1
+// value of Current Questions.
+var currentQuestion = 0;
+//value of Timer
+var count = 60
+var countdown
 
 
 
@@ -47,103 +50,131 @@ const testQuestions = [
 
 // Start Button
 function start() {
-
     var startButton = document.createElement("button")
     startButton.setAttribute("class", " col-12 btn btn-primary mt-1");
-
-
+    startButton.id = 'start-button'
 
     cardBody.appendChild(startButton)
     cardHeader.innerHTML = "Let's take a test."
     startButton.innerHTML = "START";
-
-
-
-
 }
 
 //function for button press
-function buttonPress(event) {
-
+function answerButtonPress(event) {
     var target = event.target;
 
+    if (target.matches('#start-button')) {
+        cardBody.innerHTML = ""
+        displayQuestions();
+        displayAnswers();
+        timerStart();
+        return;
+    };
+    if (target.matches('#submitButton')) {
+        storeHighScore();
+        return;
+    };
 
     if (target.matches("button")) {
-        currentQuestion++
+        var correctAns = testQuestions[currentQuestion].correctAnswer
 
-
-        if (currentQuestion < testQuestions.length) {
-
-            var correctAns = testQuestions[currentQuestion].correctAnswer
-
-            if (event.target.id !== correctAns) {
-                console.log("incorrect answer")
-            }
+        if (event.target.id !== correctAns) {
+            count -= 10;
+        }
+        if (currentQuestion < testQuestions.length - 1) {
+            currentQuestion++
             cardBody.innerHTML = ""
             displayQuestions();
             displayAnswers();
-
-
         }
         else {
-
-            cardHeader.innerHTML = "This is the high score";
-            cardBody.innerHTML = "show high score";
+            cardBody.innerHTML = ""
+            endQuiz();
         }
 
-
     }
-
 }
 
 // functions for displaying Questions.
 function displayQuestions() {
-
     cardHeader.innerHTML = testQuestions[currentQuestion].question;
-
 }
-
-
 
 // functions for displaying answers. 
 function displayAnswers() {
-
     var questionsAnswers = testQuestions[currentQuestion].answers;
 
     for (let i = 0; i < questionsAnswers.length; i++) {
-
         var questionButtons = document.createElement("button")
         cardBody.appendChild(questionButtons)
-
         questionButtons.setAttribute("class", " col-12 btn btn-primary mt-1");
         questionButtons.setAttribute("id", i);
-
-
-
         questionButtons.innerHTML = questionsAnswers[i];
-
-
-
-
-
     }
 }
 
 //function for timer
 function timerStart() {
-    var count = 60;
+    count = 60;
     var timer = document.getElementById("timer");
-    var countdown = setInterval(function () {
+    countdown = setInterval(function () {
         count--
         timer.innerHTML = count;
+
+        if (count <= 0) {
+            cardBody.innerHTML = ""
+            endQuiz();
+        }
     }, 1000)
+
+
+}
+
+//function to end quiz
+
+function endQuiz() {
+
+    var name = document.createElement("p")
+    var input = document.createElement("input");
+    var submit = document.createElement("button");
+    input.setAttribute("class", "form-control");
+    input.setAttribute("type", "text");
+    submit.setAttribute("type", "submit")
+    submit.setAttribute("class", "btn btn-primary mt-1")
+    submit.id = 'submitButton'
+    cardBody.appendChild(name);
+    cardBody.appendChild(input);
+    cardBody.appendChild(submit);
+    cardHeader.innerHTML = `<h1>High Scores</h1><h3>Your Score: ${count}</h3>`;
+    name.innerHTML = "Enter your Name."
+    input.innerHTML = "show high score";
+    submit.innerHTML = "Submit"
+    clearInterval(countdown);
+    timer.innerHTML = count;
+
+
+}
+var highScores = [];
+
+function storeHighScore() {
+
+    var highScore = {
+        name: input.value,
+        score: count
+    }
+    highScores.push(highScore);
+    localStorage.setItem("score", JSON.stringify(highScores));
 }
 
 
 
 
-// Event Listner
-cardBody.addEventListener("click", buttonPress);
 
+// Event Listner
+cardBody.addEventListener("click", answerButtonPress);
+
+// Start of program.
 
 start();
+
+
